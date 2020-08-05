751470de9f39a9929876f6ca0d965243f9072f11
peter <pete@windridge.org.uk>
2020-08-05 23:18:30 +0100

    minor typo fixes, add wall total time for ffmpeg

--- run_model_webcam.py
+++ run_model_webcam.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
+import pickle
+import os
+
 # Preprocessing function - must match what was used for training!
 from keras.applications.mobilenet_v2 import preprocess_input
 
@@ -23,32 +26,47 @@ if gpus:
 
 WEBCAM_DEV_ID = 2
 
+
+
 CLASS_NAMES = ['dontspray','spray']
 n_classes = len(CLASS_NAMES)
 
 model = load_model('/home/peter/ml/weeds/WeedML/MNv2_01-08_47.h5')
+# below is needed to stop memory leak which eventually leads to severe performance degradation (TensorFlow/Keras team pls document what is happening there!)
+# model.save("/home/peter/ml/weeds/WeedML/MNv2_01-08_47.h5", include_optimizer=False)
 model.summary()
 
 # Grid width and height
 gW = 6
 gH = 3
+
+# TODO: stop hardcoding for 224px (in case later we change to 160 for speed)
 cdX = 200 * (gW/2.0) + 12
 cdY = 200 * (gH/2.0) + 12
 
 IMG_SIZE = (224, 224)
 BATCH_SIZE = min(gW*gH,32)
 
-fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40) # for drawing
+fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40) # for annotating with predictions
 
 ## live model run from capture
+
+dest = '/home/peter/ml/weeds/demo6-nofocus/'
+if not os.path.exists(dest):
+    os.makedirs(dest)
+
+
 cap = cv2.VideoCapture(WEBCAM_DEV_ID)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+cap.set(cv2.CAP_PROP_AUTOFOCUS,0) # try disabling autofocus
+
 
 n_frames = 0
 total_time = 0
+wall_time_t0 = time.time()
 
-while(True):
+while(n_frames < 100):
     # Capture frame-by-frame
     t0 =  time.time()
     ret, frame = cap.read()
@@ -64,9 +82,7 @@ while(True):
         im = im.rotate(90,expand=True)
         w, h = im.size
 
-    #crop_x0, crop_y0 = (w/2.0 - 4 * 200 - 12, h/2.0 - 200 - 12)
     crop_x0, crop_y0 = (w/2.0 - cdX, h/2.0 - cdY)
-    #crop_x1, crop_y1 = (w/2.0 + 4 * 200 + 12, h/2.0 + 200 + 12)
     crop_x1, crop_y1 = (w/2.0 + cdX, h/2.0 + cdY)
 
     im_crop = im.crop( (crop_x0,crop_y0, min(crop_x1,w), min(crop_y1,h)))
@@ -81,10 +97,13 @@ while(True):
 
 
     im_clips = np.array(im_clips)
+
+    
+    pickle.dump( im_clips, open(dest + 'frame_{:03d}.pickle.gz'.format(n_frames) , 'wb') )
+
     t1 = time.time()
     pred = model.predict(preprocess_input(im_clips),batch_size=BATCH_SIZE)
     n_frames=n_frames+1
-
     
 
     print("Time (incl. capture): {:.2f}".format(time.time()-t0) )
@@ -103,6 +122,7 @@ while(True):
             draw.rectangle([ (x0, y0 ), (x0 + 224, y0+224)])
             draw.text((x0, y0 ), "{:.0%}".format( pred[index][1] ),font=fnt,stroke_width=1)
     
+    im_crop.save(dest + 'frame_{:03d}.png'.format(n_frames) )
     #plt.imshow(im_crop); plt.show()
     im_np = np.asarray(im_crop)
 
@@ -112,6 +132,10 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# used for constructing video with ffmpeg afterwards!
+print("Wall time: {:.2f}".format(time.time()-wall_time_t0) )
+
+# Release capture
 cap.release()
 cv2.destroyAllWindows()
+
